57711a6edda2ac98c007829f3f936b1486a76d28
heechan99 <97029480+heechan99@users.noreply.github.com>
2023-01-04 11:42:28 +0900

    Delete neural_network directory

--- neural_network/scripts/deeplabv3plus_inference.sh
+++
@@ -1,8 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python3 inference.py --model deeplabv3plus_resnet101 \
---checkpoint_path /home/download/a/log_dir \
---split test_seen \
---camera realsense \
---dataset_root /home/download/a/test_seen \
---save_dir /home/suctionnet-baseline/save_directory \
---save_visu
-
